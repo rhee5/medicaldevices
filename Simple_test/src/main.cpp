42bfccce0bba47262049b25a0c8a309a14b27aa5
Rhea K <rkripala@andrew.cmu.edu>
2025-04-09 15:00:49 -0400

    two bno055 sensors one mpu6050 sensor

--- Simple_test/src/main.cpp
+++ Simple_test/src/main.cpp
@@ -13,7 +13,10 @@
 
 
 // Create sensor object
-Adafruit_BNO055 bno = Adafruit_BNO055(55);
+// Adafruit_BNO055 bno = Adafruit_BNO055(55);
+Adafruit_BNO055 bno1 = Adafruit_BNO055(55, 0x28);
+Adafruit_BNO055 bno2 = Adafruit_BNO055(56, 0x29);
+
 Adafruit_MPU6050 mpu;
 
 // arduino to mpu6050 pins (4 pins)
@@ -35,10 +38,15 @@ void setup() {
     delay(100);
   }; 
 
-  if (!bno.begin()) {
-    Serial.println("BNO055 not detected!");
+  if (!bno1.begin()) {
+    Serial.println("BNO055 #1 not detected!");
     while (1);
   }
+  if (!bno2.begin()) {
+    Serial.println("BNO055 #2 not detected at 0x29!");
+    while (1);
+  }
+
   if (!mpu.begin()) {
     Serial.println("Initialization failed");
     return;
@@ -47,7 +55,9 @@ void setup() {
 
 
   delay(1000);
-  bno.setExtCrystalUse(true);  // Use external crystal for better accuracy
+  bno1.setExtCrystalUse(true);  // Use external crystal for better accuracy
+  bno2.setExtCrystalUse(true);
+
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
@@ -56,27 +66,47 @@ void setup() {
 }
 
 void loop() {
-  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  imu::Vector<3> euler1 = bno1.getVector(Adafruit_BNO055::VECTOR_EULER);
+  imu::Vector<3> euler2 = bno2.getVector(Adafruit_BNO055::VECTOR_EULER);
 
-  Serial.print("BNO055 Sensor: ");
+  Serial.print("BNO055 Sensor 1: ");
   Serial.print("X=");
-  Serial.print(euler.x());
+  Serial.print(euler1.x());
   Serial.print(" Y=");
-  Serial.print(euler.y());
+  Serial.print(euler1.y());
   Serial.print(" Z=");
-  Serial.println(euler.z());
+  Serial.println(euler1.z());
 
-  uint8_t system, gyro, accel, mag = 0;
-  bno.getCalibration(&system, &gyro, &accel, &mag);
+  uint8_t system1, gyro1, accel1, mag1 = 0;
+  bno1.getCalibration(&system1, &gyro1, &accel1, &mag1);
   Serial.print("CALIBRATION: Sys=");
-  Serial.print(system, DEC);
+  Serial.print(system1, DEC);
   Serial.print(" Gyro=");
-  Serial.print(gyro, DEC);
+  Serial.print(gyro1, DEC);
   Serial.print(" Accel=");
-  Serial.print(accel, DEC);
+  Serial.print(accel1, DEC);
   Serial.print(" Mag=");
-  Serial.println(mag, DEC);
+  Serial.println(mag1, DEC);
 
+
+  Serial.print("BNO055 Sensor 2: ");
+  Serial.print("X=");
+  Serial.print(euler2.x());
+  Serial.print(" Y=");
+  Serial.print(euler2.y());
+  Serial.print(" Z=");
+  Serial.println(euler2.z());
+
+  uint8_t system2, gyro2, accel2, mag2 = 0;
+  bno2.getCalibration(&system2, &gyro2, &accel2, &mag2);
+  Serial.print("CALIBRATION: Sys=");
+  Serial.print(system2, DEC);
+  Serial.print(" Gyro=");
+  Serial.print(gyro2, DEC);
+  Serial.print(" Accel=");
+  Serial.print(accel2, DEC);
+  Serial.print(" Mag=");
+  Serial.println(mag2, DEC);
 
   sensors_event_t a, g, t;
   mpu.getEvent(&a, &g, &t);
